27afcba6eaad2895b89253e6fb1e16b5ac90c132
Victorgichohi <victorgichohi48@gmail.com>
2016-01-30 17:26:53 +0300

    firebase working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
    $(".button-collapse").sideNav();
+
+  //  footer
+
+    // Send message
+    var contactFrom = document.getElementById('contact-form');
+    var contactSuccess = document.getElementById('contact-success');
+    var contactError = document.getElementById('contact-error');
+    var sendBtn = document.getElementById('send-button');
+    var onMessageComplete = function(error) {
+      sendBtn.disabled = false;
+      if (error) {
+        contactError.innerHTML = 'Sorry. Could not send message.';
+      } else {
+        contactSuccess.innerHTML = "Message has been sent.";
+
+      }
+    };
+    function sendMessage(formObj) {
+        // Store details from form to firebase
+        var myFirebaseRef = new Firebase("https://oliza.firebaseio.com/messages");
+        myFirebaseRef.push({
+          name: formObj.thename.value,
+          businessname: formObj.businessname.value,
+          email: formObj.email.value,
+          othercategoty.formObj.othercategory.value,
+          phonenumber: formObj.phonenumber.value,
+
+        }, onMessageComplete);
+        sendBtn.disabled = true;
+        return false;
+    }
